c241eefa32f06e4900702a38ffbff50f02919ad0
Carlos Wimmer <coiote777@gmail.com>
2020-04-18 19:41:47 -0300

    Apply style

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -14,9 +14,15 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: { loader: 'babel-loader' }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
       }
     ]
   }
