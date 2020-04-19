1fbeb9ed9e4b79aeeb90011cb3ac9b8e43386c85
Carlos Wimmer <coiote777@gmail.com>
2020-04-18 22:47:13 -0300

    Create post request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,17 @@ function App() {
     })
   }, [])
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo Projeto ${Date.now()}`])
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo Projeto ${Date.now()}`])
+
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: "Carlos"
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
   }
 
   return (
